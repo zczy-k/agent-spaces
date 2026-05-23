36341febb11d2f7bc3ce3eeff769a3f142fb9f0f
hunmer <liaoyanjie2000@gmail.com>
2026-05-23 22:26:55 +0800

    fix

--- packages/flutter/macos/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
+++ packages/flutter/macos/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
@@ -15,11 +15,24 @@ let package = Package(
         .library(name: "FlutterGeneratedPluginSwiftPackage", type: .static, targets: ["FlutterGeneratedPluginSwiftPackage"])
     ],
     dependencies: [
-        
+        .package(name: "url_launcher_macos", path: "../.packages/url_launcher_macos-3.2.5"),
+        .package(name: "shared_preferences_foundation", path: "../.packages/shared_preferences_foundation-2.5.6"),
+        .package(name: "package_info_plus", path: "../.packages/package_info_plus-9.0.1"),
+        .package(name: "share_plus", path: "../.packages/share_plus-10.1.4"),
+        .package(name: "path_provider_foundation", path: "../.packages/path_provider_foundation-2.5.1"),
+        .package(name: "FlutterFramework", path: "../.packages/FlutterFramework")
     ],
     targets: [
         .target(
-            name: "FlutterGeneratedPluginSwiftPackage"
+            name: "FlutterGeneratedPluginSwiftPackage",
+            dependencies: [
+                .product(name: "url-launcher-macos", package: "url_launcher_macos"),
+                .product(name: "shared-preferences-foundation", package: "shared_preferences_foundation"),
+                .product(name: "package-info-plus", package: "package_info_plus"),
+                .product(name: "share-plus", package: "share_plus"),
+                .product(name: "path-provider-foundation", package: "path_provider_foundation"),
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ]
         )
     ]
 )
